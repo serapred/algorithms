3a6eca7f48de775bec5c32ccb157b15b4e0e37cd
sergio apreda <serapred@gmail.com>
2021-05-17 14:43:15 +0200

    added new file

---
+++ grammar/datastruct.py
@@ -0,0 +1,35 @@
+# list
+"""
+lists can be used to dinamically manipulate sequences
+as they are mutable. for this reason they cause more
+overhead than tuples since memory is allocated more
+often.
+"""
+l1 = [1, 2, 3, 4, 5]
+l2 = list((1, 2, 3, 4, 5))
+
+# tuple
+"""
+tuples represent static sequences of objects. as such
+it is not possible to shallow copy a tuple (unlike lists).
+"""
+t1 = (1, 2, 3, 4, 5)
+t2 = tuple([1, 2, 3, 4, 5])
+
+# dictionary
+"""
+dictionaries stores data in key: value pairs. they are
+mutable, ordered (after 3.7) and do not allow duplicate keys.
+under the hood an hash table is used to map keys to values.
+"""
+d1 = {'a': 1, 'b': 2, 'c': 3}
+d2 = dict(a=1, b=2, c=3)
+
+# set
+"""
+sets are unordered, unchangable, unindexed, collection
+of unique items. They allow for set operations like
+subsetting, union, intersection...etc
+"""
+s1 = {1, 1, 2, 2, 3, 3}
+s2 = set((1, 1, 2, 2, 3, 3))
